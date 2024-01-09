c3eea35c4a758147ee817e03063010675e53985a
Artuom4ik <masterskayaigr123@gmail.com>
2024-01-09 20:47:17 +0300

    Add verbose_name

--- accounts/models.py
+++ accounts/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='account', verbose_name='')
+    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name='account', verbose_name='Пользователь')
     patronymic = models.CharField(max_length=200, verbose_name='Отчество')
     image = models.ImageField(default='', verbose_name='Изображение')
-    short_description = models.TextField(null=True, blank=True)
+    short_description = models.TextField(null=True, blank=True, verbose_name='Краткое описание')
 
     def __str__(self) -> str:
         return f'{self.user.last_name} {self.user.first_name}'
