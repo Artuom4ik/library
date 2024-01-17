8fd8f5bd231f3445d450cd559cf34e9cc66268b9
Artuom4ik <masterskayaigr123@gmail.com>
2024-01-17 23:00:34 +0300

    Rewrite function

--- books/management/commands/add_books.py
+++ books/management/commands/add_books.py
@@ -44,14 +44,40 @@ class Command(BaseCommand):
                 author_first_name = book['main_author']['first_name']
                 author_last_name = book['main_author']['last_name']
                 title = book['name']
-                author, create = Author.objects.get_or_create(first_name=author_first_name, last_name=author_last_name)
-                Book.objects.update_or_create(code=book_code, title=title, author=author)
+
+                try:
+                    image_url = book['default_cover']
+                    image_name = book['default_cover'].split('/')[-1]
+                    response = requests.get(f'https://i3.mybook.io/p/112x/{image_url}')
+                    response.raise_for_status()
+
+                    with open(f'media/{image_name}', 'wb') as file:
+                        file.write(response.content)
+
+                    author, create = Author.objects.get_or_create(first_name=author_first_name, last_name=author_last_name)
+                    Book.objects.update_or_create(code=book_code, title=title, image=image_name, author=author)
+
+                except:
+                    author, create = Author.objects.get_or_create(first_name=author_first_name, last_name=author_last_name)
+                    Book.objects.update_or_create(code=book_code, title=title, author=author)
         
         else:
             author_first_name = book['first_name']
             author_last_name = book['last_name']
             title = book['title']
             book_code = book['code']
-            image = book['image']
-            author, create = Author.objects.get_or_create(first_name=author_first_name, last_name=author_last_name)
-            Book.objects.update_or_create(code=book_code, title=title, image=image, author=author)
+            img_name = book['image'].split('/')[-1]
+
+            try:
+                response = requests.get(book['image'])
+                response.raise_for_status()
+
+                with open(f'media/{img_name}', 'wb') as file:
+                    file.write(response.content)
+
+                author, create = Author.objects.get_or_create(first_name=author_first_name, last_name=author_last_name)
+                Book.objects.update_or_create(code=book_code, title=title, image=img_name, author=author)
+
+            except:
+                author, create = Author.objects.get_or_create(first_name=author_first_name, last_name=author_last_name)
+                Book.objects.update_or_create(code=book_code, title=title, author=author)
